3a70866d06b047542800834b8b711cd62afbb604
mokancode <mokancode@gmail.com>
2021-05-12 20:57:22 -0400

    Refactored VideosContainer and updated 'How to update'

--- components/VideosContainer/VideoContainer.js
+++ components/VideosContainer/VideoContainer.js
@@ -1,9 +1,11 @@
 import { useContext, useState } from "react";
 import StylesContext from "../../store/styles-context";
 
-export default function VideoContainer({ video, index }) {
+export default function VideoContainer({ video, videosWidth, videosHeight }) {
   const stylesContext = useContext(StylesContext);
-  const styles = stylesContext.styles.find((styleSheet) => styleSheet.name === "VideoContainer").styles;
+  const styles = stylesContext.styles.find(
+    (styleSheet) => styleSheet.name === "VideoContainer"
+  ).styles;
   const [hideVeil, setHideVeil] = useState(false);
 
   return (
@@ -16,7 +18,15 @@ export default function VideoContainer({ video, index }) {
         setHideVeil(false);
       }}
     >
-      {video.iframe}
+      <iframe
+        width={videosWidth}
+        height={videosHeight}
+        src={video.url}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
       <div className={[styles.titleScreen, hideVeil && styles.hide].join(" ")}>
         <div className={styles.veil}>
           <h4>{video.title}</h4>
--- components/VideosContainer/VideosContainer.js
+++ components/VideosContainer/VideosContainer.js
@@ -6,75 +6,34 @@ import VideoContainer from "./VideoContainer";
 
 export default function VideosContainer() {
   const stylesContext = useContext(StylesContext);
-  const styles = stylesContext.styles.find((styleSheet) => styleSheet.name === "VideosContainer").styles;
+  const styles = stylesContext.styles.find(
+    (styleSheet) => styleSheet.name === "VideosContainer"
+  ).styles;
 
   const [IDs, setIDs] = useState([]);
 
   const videosWidth = "100%";
   const videosHeight = "100%";
-  const showVideoControls = 1;
 
   const videos = [
     {
-      iframe: (
-        <iframe
-          onClick={() => {
-            console.log("click");
-          }}
-          width={videosWidth}
-          height={videosHeight}
-          src={`https://www.youtube-nocookie.com/embed/44ANYZP5484`}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      ),
+      url: "https://www.youtube-nocookie.com/embed/44ANYZP5484",
       title: "Working with notes and emails",
     },
 
     {
-      iframe: (
-        <iframe
-          width={videosWidth}
-          height={videosHeight}
-          src={`https://www.youtube-nocookie.com/embed/5dF94iq0hAE`}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      ),
-      title: "Ardi shows Google style sign-in branding and Limited Use Requirement in Privacy Policy.",
+      url: "https://www.youtube-nocookie.com/embed/5dF94iq0hAE",
+      title:
+        "Ardi shows Google style sign-in branding and Limited Use Requirement in Privacy Policy.",
     },
 
     {
-      iframe: (
-        <iframe
-          width={videosWidth}
-          height={videosHeight}
-          src={`https://www.youtube-nocookie.com/embed/tkV_NQRlns0`}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      ),
+      url: "https://www.youtube-nocookie.com/embed/tkV_NQRlns0",
       title: "Gmail authorization and marking messages read/unread",
     },
 
     {
-      iframe: (
-        <iframe
-          width={videosWidth}
-          height={videosHeight}
-          src={`https://www.youtube-nocookie.com/embed/yoCzc_9ca58`}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      ),
+      url: "https://www.youtube-nocookie.com/embed/yoCzc_9ca58",
       title: "Ardi synchronization",
     },
   ];
@@ -91,12 +50,23 @@ export default function VideosContainer() {
 
   return (
     <div className={styles.container}>
-      <a className={styles.header} href="https://www.youtube.com/channel/UCll5lZq0reYXYz2OeSC-zbw/videos" target="_blank">
+      <a
+        className={styles.header}
+        href="https://www.youtube.com/channel/UCll5lZq0reYXYz2OeSC-zbw/videos"
+        target="_blank"
+      >
         Videos
       </a>
       <ul className={styles.list}>
         {videos.map((video, index) => {
-          return <VideoContainer key={IDs[index]} video={video} index={index} />;
+          return (
+            <VideoContainer
+              key={IDs[index]}
+              video={video}
+              videosWidth={videosWidth}
+              videosHeight={videosHeight}
+            />
+          );
         })}
       </ul>
     </div>
