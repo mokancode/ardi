2d55b9f505f2fc2a57114e1329b6870999330f22
mokancode <mokancode@gmail.com>
2021-05-14 11:46:36 -0400

    Added an anchor tag to VideoContainer to open the youtube video in a separate tab instead of playing in place

--- layout/layout.js
+++ layout/layout.js
@@ -11,7 +11,7 @@ import MobileNavbarClip from "../components/MobileNavbarClip/MobileNavbarClip";
 import classNames from "classnames";
 import { Element, animateScroll as scroll, scroller, Events } from "react-scroll";
 
-const appVersion = "1.2.8";
+const appVersion = "1.2.9";
 
 export default function Layout(props) {
   const router = useRouter();
